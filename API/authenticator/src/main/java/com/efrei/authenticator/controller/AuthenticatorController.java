ab6619b8bf0601f7975e58a2ee0da685792dcda1
Lucas Masson <lucmasson97@gmail.com>
2018-06-04 09:40:37 +0200

    Update service

--- API/authenticator/src/main/java/com/efrei/authenticator/controller/AuthenticatorController.java
+++ API/authenticator/src/main/java/com/efrei/authenticator/controller/AuthenticatorController.java
@@ -1,9 +1,16 @@
 package com.efrei.authenticator.controller;
 
+import com.efrei.authenticator.dto.AuthentificationDTO;
+import com.efrei.authenticator.exception.ResourceNotFoundException;
 import com.efrei.authenticator.model.User;
+import com.efrei.authenticator.model.Website;
 import com.efrei.authenticator.repository.UserRepository;
+import com.efrei.authenticator.repository.WebSiteRepository;
+import com.efrei.authentificator.service.AuthentificationService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +22,51 @@ public class AuthenticatorController {
 
     @Autowired
     private UserRepository userRepository;
+  
+    @Autowired
+    private WebSiteRepository websiteRepository;
+    
+    @Autowired
+    private AuthentificationService service;
 
-    @RequestMapping("/hello")
-    public String getHello(){
-        return "Hello";
+    @PostMapping("tokens")
+    public String getToken(AuthentificationDTO dto){
+    	checkNotNull(dto);
+    	return service.getToken(getUser(dto));
     }
 
-    @GetMapping("/users")
+	@PostMapping
+    public void createAccount(AuthentificationDTO dto, String websiteUrl) {
+		checkNotNull(dto);
+		
+		service.createAccount(getUser(dto),getWebSite(websiteUrl));
+	}
+
+	@GetMapping("/users")
     public List<User> getAll(){
         return userRepository.findAll();
     }
+    
+    
+    private void checkNotNull(Object dto) {
+		if(dto == null) {
+			throw new IllegalArgumentException();
+		}	
+	}
+    
+    private User getUser(AuthentificationDTO dto) {
+    	List<User> users=userRepository.findAll();
+    	
+		for(User user: users) {
+			if(user.getPassword().equals(dto.getPassword()) && user.getLogin().equals(dto.getLogin())) {
+				return user;
+			}
+		}
+		throw new ResourceNotFoundException();
+    }
+    
+    private Website getWebSite(String websiteUrl) {
+    	List<Website> websites= websiteRepository.findAll();
+		return null;
+	}
 }
---
+++ API/authenticator/src/main/java/com/efrei/authentificator/service/AuthentificationService.java
@@ -0,0 +1,18 @@
+package com.efrei.authentificator.service;
+
+import org.springframework.stereotype.Service;
+
+import com.efrei.authenticator.model.User;
+import com.efrei.authenticator.model.Website;
+
+@Service
+public class AuthentificationService {
+
+	public String getToken(User user) {
+		return user.getToken();
+	}
+
+	public void createAccount(User user, Website webSite) {
+		
+	}
+}
